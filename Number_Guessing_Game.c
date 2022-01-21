4cc96ee81153dbdccdfea3606666e61e9bece679
Muzaffar Khan <84115731+muzaffar-khan@users.noreply.github.com>
2022-01-21 13:56:00 +0530

    updated

--- Number_Guessing_Game.c
+++ Number_Guessing_Game.c
@@ -8,35 +8,6 @@ int main(){
     number = rand()%100 +1; //generate number between 1-100
     printf("The Number Is %d\n",number);
     
-    //do{
-    //    printf("Guess the number between 1 to 100\n");
-    //    scanf("%d", &guess);
-    //    if(guess>number){
-    //        printf("Lower number please!\n");
-    //    }
-    //    else if(guess<number){
-    //        printf("Higher number please!\n");
-    //    }
-    //    else{
-    //        printf("You guessed it in %d attempts\n", num_guess);
-    //    }
-    //    num_guess++;
-    //} while (guess!=number);
-
-    //for ( guess; guess!=number; num_guess++){
-    //    printf("\nGuess the number between 1 to 100\n");
-    //    scanf("%d", &guess);
-    //    if(guess>number){
-    //        printf("Lower number please!\n");
-    //    }
-    //    else if(guess<number){
-    //        printf("Higher number please!\n");
-    //    }
-    //    else{
-    //        printf("You guessed it in %d attempts\n", num_guess);
-    //    }
-    //}
-    
     while (guess!=number){
         printf("Guess the number between 1 to 100\n");
         scanf("%d", &guess);
